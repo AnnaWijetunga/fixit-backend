8d0a9637d09010ed1db9dcade28dd18c51020cae
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-08 13:45:39 -0400

    Create routes, model and controller for project

---
+++ db/migrate/20200308173316_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string "name"
+      t.string "condition"
+      t.integer "family_id"
+      t.index ["family_id"], name: "index_chores_on_family_id"
+      t.timestamps
+    end
+  end
+end
