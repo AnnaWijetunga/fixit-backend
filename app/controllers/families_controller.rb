21583ff7a7de9178e6d1e8a46277d9caeb6f237d
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-08 14:39:26 -0400

    Create routes for families controller

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,25 @@
 class FamiliesController < ApplicationController
+    def index
+        families = Family.all
+        render json: FamilySerializer.new(families).to_serialized_json
+    end
+
+    def show
+        family = Family.find_by(params[:id])
+        render json: FamilySerializer.new(family).to_serialized_json
+    end
+
+    def create
+        family = Family.new(family_params)
+
+        family.save
+        render json: family
+    end
+
+    private
+
+    def family_params
+        params.require(:family).permit(:name, :members)
+    end
+
 end
