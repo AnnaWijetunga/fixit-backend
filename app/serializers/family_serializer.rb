f38951b25f229b9abae767874de402bad2edc2a8
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-08 14:56:22 -0400

    Add FastJsonapi

--- app/serializers/family_serializer.rb
+++ app/serializers/family_serializer.rb
@@ -1,17 +1,21 @@
 class FamilySerializer
-    def initialize(family_object)
-        @family = family_object
-    end
+    include FastJsonapi::ObjectSerializer
+    attributes :name, :condition, :id, :family_id
+    # has_many :projects -- do I need this  here?
 
-    def to_serialized_json
-        options = {
-            include: {
-                projects: {
-                    only: [:name, :condition, :id, :family_id]
-                }
-            },
-            except: [:updated_at],
-        }
-        @family.to_json(options)
-    end
+    # def initialize(family_object)
+    #     @family = family_object
+    # end
+
+    # def to_serialized_json
+    #     options = {
+    #         include: {
+    #             projects: {
+    #                 only: [:name, :condition, :id, :family_id]
+    #             }
+    #         },
+    #         except: [:updated_at],
+    #     }
+    #     @family.to_json(options)
+    # end
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,17 +1,22 @@
 class ProjectSerializer
-    def initialize(project_object)
-        @project = project_object
-    end
+    include FastJsonapi::ObjectSerializer
+    attributes :name, :members
+    belongs_to :family
+    # foreign key?
 
-    def to_serialized_json
-        options = {
-            include: {
-                family: {
-                    only: [:name, :members]
-                }
-            },
-            except: [:updated_at],
-        }
-        @project.to_json(options)
-    end
+    # def initialize(project_object)
+    #     @project = project_object
+    # end
+
+    # def to_serialized_json
+    #     options = {
+    #         include: {
+    #             family: {
+    #                 only: [:name, :members]
+    #             }
+    #         },
+    #         except: [:updated_at],
+    #     }
+    #     @project.to_json(options)
+    # end
 end
