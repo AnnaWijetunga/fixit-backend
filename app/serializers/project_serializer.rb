7030666ae37b79294dfc2bb167f29829345b387a
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-09 07:55:14 -0400

    Add relationship to project serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,22 +1,5 @@
 class ProjectSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :name, :members
+    attributes :name, :condition, :id, :family_id
     belongs_to :family
-    # foreign key?
-
-    # def initialize(project_object)
-    #     @project = project_object
-    # end
-
-    # def to_serialized_json
-    #     options = {
-    #         include: {
-    #             family: {
-    #                 only: [:name, :members]
-    #             }
-    #         },
-    #         except: [:updated_at],
-    #     }
-    #     @project.to_json(options)
-    # end
 end
