56f055a65ad2f3c6790805e29f99a3941291100b
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-18 06:41:13 -0400

    Update comments for family class

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,4 @@
-# Inherits from ApplicationRecord, Project inherits from ActiveRecord
+# Inherits from ApplicationRecord, Family inherits from ActiveRecord
 # We gain methods like .all and .save because of this (i.e. Project.all)
 class Family < ApplicationRecord
     has_many :projects
